ab5c8c334416f44c97395e1ac27f41b5bf4e5fbf
Megha Bodke <megha.n.bodke@gmail.com>
2022-10-06 11:38:16 +0530

    Fixed linter error

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
-import {combineReducers} from redux;
-import { bookReducer } from "./books/books";
-import categoryRducer from "./categories/categories";
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import bookReducer from './books/books';
+import categoryRducer from './categories/categories';
 
 const rootReducer = combineReducers({
-    book:bookReducer,
-    category:categoryRducer,
+  book: bookReducer,
+  category: categoryRducer,
 });
 
-const store = configureStore({reducer:rootReducer});
- 
+const store = configureStore({ reducer: rootReducer });
+
 export default store;
