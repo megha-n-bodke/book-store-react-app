0a82ed9354d84c35dec3c3f11fca16f68461ad53
Megha Bodke <megha.n.bodke@gmail.com>
2022-10-06 11:13:25 +0530

    Configure store

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,23 +1,19 @@
-export const addBook = () => {
-  return {
-    type: "ADD_BOOK",
-  };
-};
+export const addBook = () => ({
+  type: 'ADD_BOOK',
+});
 
-export const removeBook = () => {
-  return {
-    type: "REMOVE_BOOK",
-  };
-};
+export const removeBook = () => ({
+  type: 'REMOVE_BOOK',
+});
 
 const books = [];
 
 const bookReducer = (state = books, action) => {
   switch (action.type) {
-    case "ADD_BOOK":
+    case 'ADD_BOOK':
       return [...state, action.payload];
 
-    case "REMOVE_BOOK":
+    case 'REMOVE_BOOK':
       return [...state, action.payload];
 
     default:
--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,16 +1,14 @@
 const categories = [];
-const CHECK_STATUS = "CHECK_STATUS";
+const CHECK_STATUS = 'CHECK_STATUS';
 
-export const checkStatus = () => {
-  return {
-    type: "CHECK_STATUS",
-  };
-};
+export const checkStatus = () => ({
+  type: 'CHECK_STATUS',
+});
 
 const categoryRducer = (state = categories, action) => {
   switch (action.type) {
     case CHECK_STATUS:
-      return "Under construction";
+      return 'Under construction';
     default:
       return state;
   }
