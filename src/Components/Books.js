e5fcb098dcee4350541136aab9a24f7d0207e62c
Megha Bodke <megha.n.bodke@gmail.com>
2022-10-08 22:53:26 +0530

    Style corrections

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -17,13 +17,19 @@ const Books = (props) => {
           <p className="category">{type}</p>
           <p className="Title">{title}</p>
           <p className="author">{author}</p>
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={clickHandler}
-          >
-            Remove
-          </button>
+          <div className="action-buttons">
+            <span type="button" className="Remove">
+              Comments
+            </span>
+            <div class="Line-2"></div>
+            <span type="button" onClick={clickHandler} className="Remove">
+              Remove
+            </span>
+            <div class="Line-2"></div>
+            <span type="button" className="Remove">
+              Edit
+            </span>
+          </div>
         </div>
         {/* progress bar try */}
         <div className="middle-section">
