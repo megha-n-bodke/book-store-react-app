82c35989590d37aa38f4726a27a514c8bfef86b5
Megha Bodke <megha.n.bodke@gmail.com>
2022-10-08 13:49:49 +0530

    Add style to book component

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/books';
-
+import React from "react";
+import { useDispatch } from "react-redux";
+import { removeBook } from "../redux/books/books";
+import "./book.css";
 const Books = (props) => {
-  const {
-    type, title, author, id,
-  } = props;
+  const { type, title, author, id } = props;
   const dispatch = useDispatch();
   const clickHandler = () => {
     dispatch(removeBook(id));
   };
 
   return (
-    <div className="d-flex flex-column align-items-center">
-      <div className="border border-secondary rounded  w-75 my-3 ps-5">
-        <p className="text-left">{type}</p>
-        <p>{title}</p>
-        <p>{author}</p>
-
-        <button
-          type="button"
-          className="btn btn-primary"
-          onClick={clickHandler}
-        >
-          Remove
-        </button>
+    <div>
+      <div className="book-card">
+        <div>
+          {" "}
+          <p className="category">{type}</p>
+          <p className="Title">{title}</p>
+          <p className="author">{author}</p>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={clickHandler}
+          >
+            Remove
+          </button>
+        </div>
+        <div class="Oval-2"></div>
       </div>
     </div>
   );
